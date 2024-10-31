733d96935d1aa9076938cf9d707f2d17c708d3f4
Nicolas de Sá <nicolasdesacs@gmail.com>
2024-10-31 11:25:00 -0300

    Conclusão da função draw
    usada para desenhar os todo elementos do jogo (jogadores, bola e campo)

--- game.cpp
+++ game.cpp
@@ -19,12 +19,35 @@ void setup() { //Função para deifinir as posições padrões dos jogadores e b
     posicaojogador2y = alturadocampo / 2;
 }
 
-// Função para desenhar o campo.
+// Função para desenhar o campo e jogadores.
 void draw() {
     system("cls"); // Limpa a tela do terminal.
+
+    // Desenhando elementos do jogo
+    for (int i =0; i < alturadocampo; i++) { // Desenhando altura do campo
+        for (int j = 0; j < larguradocampo; j++) { // Desenhando largura do campo
+            if (j == 0) { // Verificando se o eixo X esta na posição 0 do campo para podermos desenhar o jogador1
+                if (i == posicaojogador1y) std::cout << "|"; // Desenha o jogador1 quando chegar na metade do campo no eixo Y
+                else std::cout << " "; // Desenha espaços vazios no eixo Y quando não estiver no meio do campo
+            }
+            else if (j == larguradocampo - 1) { // Verifica se a posição X do campo esta na ultima casa para desenharmos o jogador2
+                if (i == posicaojogador2y) std::cout << "|"; // Desenha o jogador2 quando chegar na metade do campo no eixo Y
+                else std::cout << "|"; // Desenha espaços vazios no eixo Y quando não estiver no meio do campo
+            }
+            else if (j == posicaobolax && i == posicaobolay) { // Verifica se o eixo X e Y estão no meio do campo para desenhar a bola
+                std:cout << "O"; // Desenhar a bola
+            }
+            else {
+                std::cout << " "; // Desenha espaço vazio quando não se encaixa em nenhum dos parâmetros acima
+            }
+        }
+        std::cout << "\n"; // Ao desenhar uma linha do campo pula para a próxima linha (para continuar desenhando o campo)
+    }
 }
 
+// Função com o jogo
 int main() {
+    setup(); // Chamando a posição inicial de todos os elementos do jogo
     while(gameloop) {
         input();
         simulate();
